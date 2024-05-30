47bb144be1515d3466b82a6ae082a87065d67e58
camimcgee <166550495+camimcgee@users.noreply.github.com>
2024-05-30 15:56:51 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -38,3 +38,21 @@ def prior_prob(table, target, targetVal):
   columnList = up_get_column(table, target)
   pA = sum([1 if i == targetVal else 0 for i in columnList])/len(columnList)
   return pA
+
+def metrics(zipped_list):
+  tn = sum([1 if pair==[0,0] else 0 for pair in zipped_list])
+  tp = sum([1 if pair==[1,1] else 0 for pair in zipped_list])
+  fp = sum([1 if pair==[1,0] else 0 for pair in zipped_list])
+  fn = sum([1 if pair==[0,1] else 0 for pair in zipped_list])
+
+  precision = tp/(tp+fp) if (tp+fp) else 0
+  recall = tp/(tp+fn) if (tp+fn) else 0
+  F1 = 2*((precision*recall)/(precision+recall)) if (precision+recall) else 0
+  accuracy = (tp+tn)/(tp+tn+fp+fn) if (tp+tn+fp+fn) else 0
+  predici_dict = {
+      'Precision': precision,
+      'Recall': recall,
+      'F1': F1,
+      'Accuracy': accuracy
+  }
+  return predici_dict
